4974cee70970d11cc889d2e877172b1a20ee619d
Cydney Caldwell <caldwellc1@appstate.edu>
2017-04-23 19:34:49 -0400

    current stats added

--- Report.java
+++ Report.java
@@ -50,6 +50,8 @@ public class Report
                         printSpriteCounts(printW, i);
                     }
                 }
+                printW.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                printW.println();
             }
             printW.close();
         }
@@ -69,12 +71,30 @@ public class Report
     {
         printW.println("File: " + submissions[i].getName());
         printW.println("---------------------------------");
-        printW.println("Script Count Total: " 
+        printW.println("Script Count Total: \t" 
             + submissions[i].getScriptCount());
-        printW.println("Sprite Count Total: " 
+        printW.println("Sprite Count Total: \t" 
             + submissions[i].getSpriteCount());
-        printW.println();
-        printW.println("---------------------------------");
+        printW.println("Control Blocks: \t\t" 
+            + submissions[i].getControlBlocksForProgram());
+        printW.println("Data Blocks: \t\t\t" 
+            + submissions[i].getDataBlocksForProgram());
+        printW.println("Event Blocks: \t\t\t" 
+            + submissions[i].getEventsBlocksForProgram());
+        printW.println("Looks Blocks: \t\t\t" 
+            + submissions[i].getLooksBlocksForProgram());
+        printW.println("More Blocks Blocks: \t" 
+            + submissions[i].getMoreBlocksBlocksForProgram());
+        printW.println("Motion Blocks: \t\t\t" 
+            + submissions[i].getMotionBlocksForProgram());
+        printW.println("Operator Blocks: \t\t" 
+            + submissions[i].getOperatorsBlocksForProgram());
+        printW.println("Pen Blocks: \t\t\t" 
+            + submissions[i].getPenBlocksForProgram());
+        printW.println("Sensing Blocks: \t\t" 
+            + submissions[i].getSensingBlocksForProgram());
+        printW.println("Sound Blocks: \t\t\t" 
+            + submissions[i].getSoundBlocksForProgram());
         printW.println();
     }
 
@@ -87,38 +107,39 @@ public class Report
     private void printStageCounts(PrintWriter printW, int i)
     {
         printW.println("Stage Counts");
-        printW.println("Scripts: " 
+        printW.println("---------------------------------");
+        printW.println("Scripts: \t\t\t\t" 
             + submissions[i].getScriptCountForStage());
-        printW.println("Variables: " 
+        printW.println("Variables: \t\t\t\t" 
             + submissions[i].getVariableCountForStage());
-        printW.println("ScriptComments: " 
+        printW.println("Lists: \t\t\t\t\t" 
+                    + submissions[i].getListCountForStage());
+        printW.println("ScriptComments: \t\t" 
             + submissions[i].getScriptCommentCountForStage());
-        printW.println("Sounds: " 
+        printW.println("Sounds: \t\t\t\t" 
             + submissions[i].getSoundCountForStage());
-        printW.println("Costumes: " 
+        printW.println("Costumes: \t\t\t\t" 
             + submissions[i].getCostumeCountForStage());
-        printW.println("Control Blocks: " 
+        printW.println("Control Blocks: \t\t" 
             + submissions[i].getControlBlocksForStage());
-        printW.println("Data Blocks: " 
+        printW.println("Data Blocks: \t\t\t" 
             + submissions[i].getDataBlocksForStage());
-        printW.println("Event Blocks: " 
+        printW.println("Event Blocks: \t\t\t" 
             + submissions[i].getEventsBlocksForStage());
-        printW.println("Looks Blocks: " 
+        printW.println("Looks Blocks: \t\t\t" 
             + submissions[i].getLooksBlocksForStage());
-        printW.println("More Blocks Blocks: " 
+        printW.println("More Blocks Blocks: \t" 
             + submissions[i].getMoreBlocksBlocksForStage());
-        printW.println("Motion Blocks: " 
+        printW.println("Motion Blocks: \t\t\t" 
             + submissions[i].getMotionBlocksForStage());
-        printW.println("Operator Blocks: " 
+        printW.println("Operator Blocks: \t\t" 
             + submissions[i].getOperatorsBlocksForStage());
-        printW.println("Pen Blocks: " 
+        printW.println("Pen Blocks: \t\t\t" 
             + submissions[i].getPenBlocksForStage());
-        printW.println("Sensing Blocks: " 
+        printW.println("Sensing Blocks: \t\t" 
             + submissions[i].getSensingBlocksForStage());
-        printW.println("Sound Blocks: " 
+        printW.println("Sound Blocks: \t\t\t" 
             + submissions[i].getSoundBlocksForStage());
-        printW.println();
-        printW.println("--------------------------------");
         printW.println();
     }
 
@@ -131,16 +152,43 @@ public class Report
     private void printSpriteCounts(PrintWriter printW, int i)
     {
         printW.println("Sprite Counts");
+        printW.println("---------------------------------");
         Sprite[] sprites = submissions[i].getSprites();
         for (int j = 0; j < sprites.length; j++)
         {
             printW.println("Sprite: " + sprites[j].getName());
-            printW.println("Scripts: " + sprites[j].getScriptCount());
-            printW.println("Variables: " + sprites[j].getVariableCount());
-            printW.println("ScriptComments: " 
+            printW.println("Scripts: \t\t\t\t" 
+                + sprites[j].getScriptCount());
+            printW.println("Variables: \t\t\t\t" 
+                + sprites[j].getVariableCount());
+            printW.println("Lists: \t\t\t\t\t" 
+                + sprites[j].getListCount());
+            printW.println("ScriptComments: \t\t" 
                 + sprites[j].getScriptCommentCount());
-            printW.println("Sounds: " + sprites[j].getSoundCount());
-            printW.println("Costumes: " + sprites[j].getCostumeCount());
+            printW.println("Sounds: \t\t\t\t" 
+                + sprites[j].getSoundCount());
+            printW.println("Costumes: \t\t\t\t" 
+                + sprites[j].getCostumeCount());
+            printW.println("Control Blocks: \t\t" 
+                + sprites[j].getControlBlocksForSprite());
+            printW.println("Data Blocks: \t\t\t" 
+                + sprites[j].getDataBlocksForSprite());
+            printW.println("Event Blocks: \t\t\t" 
+                + sprites[j].getEventsBlocksForSprite());
+            printW.println("Looks Blocks: \t\t\t" 
+                + sprites[j].getLooksBlocksForSprite());
+            printW.println("More Blocks Blocks: \t" 
+                + sprites[j].getMoreBlocksBlocksForSprite());
+            printW.println("Motion Blocks: \t\t\t" 
+                + sprites[j].getMotionBlocksForSprite());
+            printW.println("Operator Blocks: \t\t" 
+                + sprites[j].getOperatorsBlocksForSprite());
+            printW.println("Pen Blocks: \t\t\t" 
+                + sprites[j].getPenBlocksForSprite());
+            printW.println("Sensing Blocks: \t\t" 
+                + sprites[j].getSensingBlocksForSprite());
+            printW.println("Sound Blocks: \t\t\t" 
+                + sprites[j].getSoundBlocksForSprite());
             printW.println();
         }
     }   
